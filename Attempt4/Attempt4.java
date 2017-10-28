1c2bf0c85844d4c15f4994b599be436255eac345
Josh Kelso <scottkelso07@hotmail.com>
2017-10-28 11:32:13 +0100

    Modified Attempt 4

--- Attempt4/Attempt4.java
+++ Attempt4/Attempt4.java
@@ -9,16 +9,17 @@
     G McClements 30/6/99; PLK February 2000; NSS July 2001;
     HV September 2014
 ===========================================================*/
+
 import java.util.Random;
 
 class Time {
-    public static void delay( int msec ) {
-	// Pause thread for specified number of milliseconds
-	try {
-	    Thread.sleep( msec );
-	} catch( InterruptedException e ) {
-	    Thread.currentThread().interrupt();
-	}
+    public static void delay(int msec) {
+        // Pause thread for specified number of milliseconds
+        try {
+            Thread.sleep(msec);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
     }
 }
 
@@ -27,7 +28,7 @@ class Attempt4 {
     public static volatile int c1 = 1;
     public static volatile int c2 = 1;
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         P1 thread1 = new P1();
         P2 thread2 = new P2();
@@ -46,42 +47,42 @@ class P1 extends Thread {
             preProtocol();
             criticalSection();
             postProtocol();
-       }
+        }
     }
 
     public void nonCriticalSection() {
     }
 
     public void preProtocol() {
-       System.out.println("1 prep: in preProtocol ");
-       // yield();
-	Time.delay(rnd.nextInt(120));
-       Attempt4.c1 = 0;
-       // yield();
-	Time.delay(rnd.nextInt(120));
-       while (Attempt4.c2 != 1) {
-       // yield();
-	Time.delay(rnd.nextInt(120));
-       Attempt4.c1=1;
-       yield();
-//       Time.delay(rnd.nextInt(120));
-       Attempt4.c1=0;
-       // yield();
-	Time.delay(rnd.nextInt(120));
-       }
-       System.out.println("1 prep: leaving preProtocol ");
+        System.out.println("1 prep: In preProtocol ");
+//        yield();
+        Time.delay(rnd.nextInt(120));
+        Attempt4.c1 = 0;
+//        yield();
+        Time.delay(rnd.nextInt(120));
+        while (Attempt4.c2 != 1) {
+//            yield();
+            Time.delay(rnd.nextInt(120));
+            Attempt4.c1 = 1;
+            yield();
+//            Time.delay(rnd.nextInt(120));
+            Attempt4.c1 = 0;
+//            yield();
+            Time.delay(rnd.nextInt(120));
+        }
+        System.out.println("1 prep: leaving preProtocol ");
     }
 
     public void criticalSection() {
-       System.out.println("1 cs: Entering critical section ");
-       System.out.println("1 cs: In critical section ");
-       Time.delay(rnd.nextInt(120));
-       System.out.println("1 cs: Leaving critical section ");
+        System.out.println("1 cs: Entering critical section ");
+        System.out.println("1 cs: In critical section ");
+        Time.delay(rnd.nextInt(120));
+        System.out.println("1 cs: Leaving critical section ");
     }
 
     public void postProtocol() {
-       Attempt4.c1 = 1;
-       Time.delay(rnd.nextInt(120));
+        Attempt4.c1 = 1;
+        Time.delay(rnd.nextInt(120));
     }
 }
 
@@ -94,41 +95,41 @@ class P2 extends Thread {
             preProtocol();
             criticalSection();
             postProtocol();
-    }
+        }
     }
 
     public void nonCriticalSection() {
     }
 
     public void preProtocol() {
-       System.out.println("2 prep: In preProtocol ");
-       // yield();
-	Time.delay(rnd.nextInt(120));
-       Attempt4.c2 = 0;
-       // yield();
-	Time.delay(rnd.nextInt(120));
-       while (Attempt4.c1 != 1) {
-       // yield();
-	Time.delay(rnd.nextInt(120));
-       Attempt4.c2=1;
-       yield();
-//       Time.delay(rnd.nextInt(120));
-       Attempt4.c2=0;
-       // yield();
-	Time.delay(rnd.nextInt(120));
-       }
-       System.out.println("2 prep: Leaving preProtocol ");
+        System.out.println("2 prep: In preProtocol ");
+//        yield();
+        Time.delay(rnd.nextInt(120));
+        Attempt4.c2 = 0;
+//        yield();
+        Time.delay(rnd.nextInt(120));
+        while (Attempt4.c1 != 1) {
+//            yield();
+            Time.delay(rnd.nextInt(120));
+            Attempt4.c2 = 1;
+            yield();
+//            Time.delay(rnd.nextInt(120));
+            Attempt4.c2 = 0;
+//            yield();
+            Time.delay(rnd.nextInt(120));
+        }
+        System.out.println("2 prep: Leaving preProtocol ");
     }
 
     public void criticalSection() {
-       System.out.println("2 cs: Entering critical section");
-       System.out.println("2 cs: In critical section");
-       Time.delay(rnd.nextInt(120));
-       System.out.println("2 cs: Leaving critical section");
+        System.out.println("2 cs: Entering critical section");
+        System.out.println("2 cs: In critical section");
+        Time.delay(rnd.nextInt(120));
+        System.out.println("2 cs: Leaving critical section");
     }
 
     public void postProtocol() {
-       Attempt4.c2 = 1;
-       Time.delay(rnd.nextInt(120));
+        Attempt4.c2 = 1;
+        Time.delay(rnd.nextInt(120));
     }
 }
